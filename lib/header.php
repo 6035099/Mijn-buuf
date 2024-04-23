beaff3f727dc28071404a433f79581edcb9603f4
Santos6035983 <6035983@mborijnland.nl>
2024-04-23 15:10:38 +0200

    Update header.php

--- lib/header.php
+++ lib/header.php
@@ -10,11 +10,9 @@
     
     <header>
         <h1>Welcome to Mijn Buuf</h1>
-        <nav>
+        <nav class="nav">
             <a href="#">Home</a>
             <a href="#">About</a>
             <a href="contact.php">Contact</a>
         </nav>
     </header>
-            
-            
