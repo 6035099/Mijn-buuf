caa252cecbe1a9c4430f2074563ff9c02ba2f1c8
Santos6035983 <6035983@mborijnland.nl>
2024-04-23 15:21:19 +0200

    Update contact.php

--- contact.php
+++ contact.php
@@ -4,10 +4,10 @@
     <main>
         <article class="info">
             <section class="contact-gegevens">
-                <h2>Contactgegevens</h2>
-                <p>E-mailadres: </b> MijnBuuf@gmail.com</p>
-                <p>Telefoonnummer: </b> +31 612345749</p>
-                <p>Waar?:</b> Bleiswijkseweg 37E, 2712 PB Zoetermeer</p>
+                <h1>Contactgegevens</h1>
+                <p><b>E-mailadres: </b> MijnBuuf@gmail.com</p>
+                <p><b>Telefoonnummer: </b> +31 612345749</p>
+                <p><b>Waar?:</b> Bleiswijkseweg 37E, 2712 PB Zoetermeer</p>
             </section>
 
             <section class="Contactin">
@@ -24,25 +24,25 @@
 
 
         <section class="contact-formulier">
-            <h2>Contactformulier</h2>
+            <h1>Contactformulier</h1>
             <p>We reageren binnen 24 uur</p>
             <form action="#">
-                <label for="naam">Naam:</label>
+                <label for="naam"><b>Naam:</b></label>
                 <input type="text" id="naam" name="naam" required>
 
-                <label for="email">E-mailadres:</label>
+                <label for="email"><b>E-mailadres:</b></label>
                 <input type="email" id="email" name="email" required>
 
-                <label for="Tele">Telefoonnummer:</label>
+                <label for="Tele"><b>Telefoonnummer:</b></label>
                 <input type="Tele" id="Tele" name="Tele" required>
 
-                <label for="onderwerp">Onderwerp:</label>
+                <label for="onderwerp"><b>Onderwerp:</b></label>
                 <input type="text" id="onderwerp" name="onderwerp" required>
 
-                <label for="bericht">Bericht:</label>
+                <label for="bericht"><b>Bericht:</b></label>
                 <textarea id="bericht" name="bericht" required></textarea>
 
-                <button type="submit">Verzenden</button>
+                <button type="submit"><b>Verzenden</b></button>
             </form>
         </section>
     </main>
